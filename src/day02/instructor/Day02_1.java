e936f36f94c0c4896b852f1583b7e8598e40e23b
Kubanychbek Zhaanbaev <k.zhaanbaev4@gmail.com>
2023-01-05 19:16:57 -0500

    day02 instructor folder added

---
+++ src/day02/instructor/Day02_1.java
@@ -0,0 +1,23 @@
+package day02.instructor;
+
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+
+public class Day02_1 {
+    public static void main(String[] args) throws InterruptedException {
+        //1. Connect the program with the chromedriver and geckodriver
+
+        //2. Launch a browser
+
+        //3. holds program until n number of milliSeconds
+
+        //4. get title of the page
+
+        //5. get current url of the webpage
+
+        //6. close the browser
+    }
+
+
+
+}
