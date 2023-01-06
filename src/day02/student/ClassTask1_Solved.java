8f1f849cc454c401beae5eec1ddbf5198c7964b5
Kubanychbek Zhaanbaev <k.zhaanbaev4@gmail.com>
2023-01-05 21:29:32 -0500

    solved task 1

---
+++ src/day02/student/ClassTask1_Solved.java
@@ -0,0 +1,45 @@
+package day02.student;
+
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+
+public class ClassTask1_Solved {
+    /**
+     * Set up WebDriver object
+     *
+     * Open https://amazon.com
+     * Apply 2 seconds wait
+     * Print out following:
+     *      Title
+     *      URL
+     *      Length of the URL
+     *
+     * Open https://tesla.com
+     * Apply 2 seconds wait
+     * Print out following:
+     *      Title
+     *      URL
+     *      Length of the URL
+     * Close the browser
+     */
+    public static void main(String[] args) throws InterruptedException {
+        System.setProperty("webdriver.chrome.driver", "/Users/kuba/TLA/Selenium/B-7/libs/drivers/chromedriver");
+        WebDriver driver = new ChromeDriver();
+        driver.get("https://google.com");
+
+        driver.get("https://amazon.com");
+        Thread.sleep(2000);
+        System.out.println(driver.getTitle());
+        System.out.println(driver.getCurrentUrl());
+        System.out.println(driver.getCurrentUrl().length());
+
+        driver.get("https://tesla.com");
+        Thread.sleep(2000);
+        System.out.println(driver.getTitle());
+        System.out.println(driver.getCurrentUrl());
+        System.out.println(driver.getCurrentUrl().length());
+
+        driver.close();
+
+    }
+}
---
+++ src/day02/student/Day02_1_Student.java
@@ -0,0 +1,2 @@
+package day02.student;public class Day02_1_Student {
+}
