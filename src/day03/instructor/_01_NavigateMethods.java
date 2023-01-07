ccbdb1eac47a20f9881aa3bca420e61439d80e8a
dermg13 <dermg13@gmail.com>
2023-01-06 19:33:10 -0500

    added_day03

---
+++ src/day03/instructor/_01_NavigateMethods.java
@@ -0,0 +1,23 @@
+package day03.instructor;
+
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+
+public class _01_NavigateMethods {
+    public static void main(String[] args) {
+        System.setProperty("webdriver.chrome.driver", "C:\\Users\\mder\\OneDrive\\Documents\\Selenium\\Drivers\\chromedriver_win32_updated\\chromedriver.exe");
+        WebDriver driver = new ChromeDriver();
+
+        //1. Navigate methods
+
+        //action 2 to click on one of the videos
+
+        //2. navigate() vs get()
+
+
+        //action 2 to click on one of the videos
+
+
+    }
+}
+
---
+++ src/day03/instructor/_02_BasicLocators.java
@@ -0,0 +1,29 @@
+package day03.instructor;
+
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+
+public class _02_BasicLocators {
+    public static void main(String[] args) {
+        System.setProperty("webdriver.chrome.driver", "C:\\Users\\mder\\OneDrive\\Documents\\Selenium\\Drivers\\chromedriver_win32_updated\\chromedriver.exe");
+        WebDriver driver = new ChromeDriver();
+
+        //locate by id
+
+
+        //locate by name
+
+
+        //locate by className
+
+
+        //locate by tagName
+
+
+        //locate by linkText
+
+
+        //locate by partialLinkText
+
+    }
+}
