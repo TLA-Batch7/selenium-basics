9963137b578efab58f557a8f22bf41eb4ab65ff7
dermg13 <dermg13@gmail.com>
2023-01-08 12:13:22 -0500

    updated day04 instructor

--- src/day04/instructor/_01_BasicInteractions.java
+++ src/day04/instructor/_01_BasicInteractions.java
@@ -19,33 +19,55 @@ public class _01_BasicInteractions {
          */
 
         //Set up the driver
+        System.setProperty("webdriver.chrome.driver", "C:\\Users\\mder\\OneDrive\\Documents\\Selenium\\Drivers\\chromedriver_win32_updated\\chromedriver.exe");
+        WebDriver driver = new ChromeDriver();
+        driver.manage().window().maximize();
+
 
         //1.
-
+        driver.get("https://google.com");
 
         //2. How to INPUT a text in input fields?
-
+        driver.findElement(By.name("q")).sendKeys("selenium");
 
         //3. How to CLICK on a button?
-
+        driver.findElement(By.name("btnK")).click();
 
         //4. How to get value of any attribute of the element?
-
+        String href = driver.findElement(By.linkText("Wikipedia")).getAttribute("href");
+        System.out.println(href);
 
         //4.1 Verify expected vs actual
-
+        String expectedHref = "https://en.wikipedia.org/wiki/Selenium";
+        if (href.equals(expectedHref)){
+            System.out.println("PASS");
+        } else {
+            System.out.println("FAIL");
+        }
 
         //5. Print out header of the first item from the search result
-
+        String actualResult = driver.findElement(By.tagName("h3")).getText();
+        System.out.println(actualResult);
 
         //5.1 Verify if word contains a word "selenium"
+        String expectWord = "selenium";
+        if (actualResult.toLowerCase().contains(expectWord)){
+            System.out.println("PASS");
+        } else {
+            System.out.println("Search result doesn't contain the expected result");
+        }
 
 
         //6. Verify if button "Shopping" is there. (is enabled)
-
+        System.out.println(driver.findElement(By.linkText("Shopping")).isEnabled());
 
         //7. Verify if google logo on top left of the page is displayed
+        if (driver.findElement(By.id("logo")).isDisplayed()){
+            System.out.println("PASS");
+        } else {
+            System.out.println("FAIL");
+        }
 
-
+        driver.close();
     }
 }
